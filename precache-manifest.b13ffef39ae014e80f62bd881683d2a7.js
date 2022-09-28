f4a2f143c1b7998970b0144a64814ff8534aed5f
ashu3110 <avsingh000.789@gmail.com>
2022-09-28 11:01:20 +0530

    first commit

---
+++ precache-manifest.b13ffef39ae014e80f62bd881683d2a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "037278f3a89f8d5e7a9a",
+    "url": "./static/css/main.56c47830.chunk.css"
+  },
+  {
+    "revision": "037278f3a89f8d5e7a9a",
+    "url": "./static/js/main.037278f3.chunk.js"
+  },
+  {
+    "revision": "6a7bfa4df9242b40edff",
+    "url": "./static/js/1.6a7bfa4d.chunk.js"
+  },
+  {
+    "revision": "a2bfa05939825a50cd15",
+    "url": "./static/js/2.a2bfa059.chunk.js"
+  },
+  {
+    "revision": "44bb06a9c102f76212e7",
+    "url": "./static/js/runtime~main.44bb06a9.js"
+  },
+  {
+    "revision": "f22cabf1b32eb7f9f9e4cb92221315e5",
+    "url": "./index.html"
+  }
+];
